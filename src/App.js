e53ce921242a7eac7b8e54f2fbfe2762aa927660
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2023-01-06 17:40:33 +0530

    Refactor:Event Binding is used to handle the event raised from the DOM like button click

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import './App.css';
 import logo from './logo.svg'
 
 class App extends React.Component {
-
+  url = "https://www.google.com/"
   constructor(){
     super()
     this.state={
       title:"Hello From Bridgelabz"
     }
   }
+
+  //onClick Function
+  OnClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
   render(){
   return (
     <div className="App">
       <h2>{this.state.title}</h2>
-      <img src={logo} />
+      <img src={logo} OnClick={this.OnClick}  
+        alt='The Bridgelabz logo: a Bridge to Employement through lab Works'/>
       </div>
   );
 }
