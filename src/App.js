5e542ddfe2df5a63746d044032a27004df8b085d
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2023-01-06 17:59:21 +0530

    Refactor:Display Hello From Bridgelabz with a user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(){
     super()
     this.state={
-      title:"Hello From Bridgelabz"
+      userName:''
     }
   }
 
@@ -16,12 +16,22 @@ class App extends React.Component {
     console.log("save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
+
+  onNameChange = (event) => {
+    console.log("value is ",event.target.value);
+    //set the title using setState method
+    this.setState({ userName:event.target.value})
+  }
+
   render(){
   return (
     <div className="App">
-      <h2>{this.state.title}</h2>
+      <h2>Hello {this.state.userName} from Bridgelabz</h2>
       <img src={logo} OnClick={this.OnClick}  
         alt='The Bridgelabz logo: a Bridge to Employement through lab Works'/>
+        <div>
+          <input onChange={this.onNameChange} />
+        </div>
       </div>
   );
 }
